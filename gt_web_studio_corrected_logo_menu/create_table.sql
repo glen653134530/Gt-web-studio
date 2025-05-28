71455c1ad76848b6a171b3933446553cb1c6cae8
glen653134530 <tchouamoglen237@gmail.com>
2025-05-28 18:02:58 +0200

    Delete gt_web_studio_corrected_logo_menu directory

--- gt_web_studio_corrected_logo_menu/create_table.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS paniers (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom_produit VARCHAR(255),
-    prix INT,
-    date_ajout DATETIME
-);
--- gt_web_studio_corrected_logo_menu/create_utilisateurs.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS utilisateurs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255),
-    email VARCHAR(255),
-    password VARCHAR(255),
-    date_creation DATETIME
-);
